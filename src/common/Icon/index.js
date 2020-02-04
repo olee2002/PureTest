e5cdeea34b159cdfbf1ed07d6e05a9f32f01d9d1
Eugene Kim <ekim1707@gmail.com>
2020-02-03 23:12:18 -0500

    update

--- src/common/Icon/index.js
+++ src/common/Icon/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Icon = ({ className, iconName }) => {
-    const style = { background: `url('../../img/${iconName}.png') 0% 0% / contain no-repeat` };
+const Icon = ({ className, iconName, cover }) => {
+    const style = cover ? { background: `url('../../img/${iconName}.png') 0% 0% / cover no-repeat` } : 
+    { background: `url('../../img/${iconName}.png') 0% 0% / contain no-repeat` };
     return <div className={className} style={style} ></div>
 };
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,15 +8,42 @@ import FooterNav from "../FooterNav";
 const Home = (props) => {
     let [interval, setIntervalFunction] = useState(0);
     setInterval(() => {setIntervalFunction(interval = interval === 4 ? 0 : interval + 1);}, 5000);
+    const [clickCount, setClickCount] = useState(0);
+    const handleArrowClick = (type) => {
+        type === "left" ? setClickCount(clickCount + 1) : setClickCount(clickCount - 1);
+    }
+    console.log(clickCount);
     return (
         <div className="homeContainer">
             <NavContainer />
             <section className="headerContainer">
                 <h1 className="headerLabels">{content.HEADER_ARRAY[interval]}</h1>
-                <div className="photoGallery">
-                    <Icon className="arrowLeftIcon" iconName="arrow_left" />
-                    <Icon className="arrowRightIcon" iconName="arrow_right" />
-                    Images go here
+                <div className="photoGalleryCarousel">
+                    <div className="photoGalleryLeftSide"></div>
+                    <div className="photoGalleryWindow">
+                        {clickCount < 6 && (
+                            <div onClick={() => handleArrowClick("left")} className="iconContainer">
+                                <Icon className="arrowLeftIcon" iconName="arrow_left" />
+                            </div>
+                        )};
+                        <div className="photoRow" style={{ transform: `translateX(calc(50% - 1050px - (700px * ${clickCount})))`}}>
+                            <Icon className="photo" iconName="gallery_one" cover={true} />
+                            <Icon className="photo" iconName="gallery_two" cover={true} />
+                            <Icon className="photo" iconName="gallery_three" cover={true} />
+                            <Icon className="photo" iconName="gallery_four" cover={true} />
+                            <Icon className="photo" iconName="gallery_five" cover={true} />
+                            <Icon className="photo" iconName="gallery_six" cover={true} />
+                            <Icon className="photo" iconName="gallery_seven" cover={true} />
+                            <Icon className="photo" iconName="gallery_eight" cover={true} />
+                        </div>
+                        {clickCount > -1 && (
+                            <div onClick={() => handleArrowClick("right")}  className="iconContainer">
+                                <Icon className="arrowRightIcon" iconName="arrow_right" />
+                            </div>
+                        )};
+                    </div>
+                    <div className="photoGalleryWindowBorder"></div>
+                    <div className="photoGalleryRightSide"></div>
                 </div>
             </section>
             <section className="bodyContainer">
